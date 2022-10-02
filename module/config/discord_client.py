cdb37ef0582578b9f72e4eb800f8d4207f206ce8
Edgar Marques <edgarmarques.110@gmail.com>
2022-10-02 14:52:56 -0300

    feat: add base code

---
+++ module/config/discord_client.py
@@ -0,0 +1,18 @@
+import os
+import discord
+
+class DiscordClient:
+  client = None
+
+  def __init__(self):
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+    client = discord.Client(intents=discord.Intents.default())
+
+    @client.event
+    async def on_ready():
+      print(f'{client.user} has connected to Discord!')
+
+    client.run(DISCORD_TOKEN)
+
+    self.client = client
---
+++ module/core.py
@@ -0,0 +1,6 @@
+from main.application import Application
+import config.dotenv
+
+app = Application()
+
+app.run()
