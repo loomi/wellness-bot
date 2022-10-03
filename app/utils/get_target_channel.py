11100dbf9ed7f40f36b87a4b530a4bdf86a70313
Edgar Marques <edgarmarques.110@gmail.com>
2022-10-02 21:19:06 -0300

    fix: remove unused utils

--- app/utils/get_target_channel.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-
-def get_target_channel(client: discord.Client):
-    channels = client.get_all_channels()
-
-    target_channel = discord.utils.get(channels, name='dia-de-office')
-
-    return target_channel
--- app/utils/weekdays.py
+++
@@ -1,2 +0,0 @@
-weekdays = ['Segunda-feira', 'Terça-feira', 'Quarta-feira',
-            'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']
